a446a31f5f019d7c38d5a7110b2778e92632d1a6
Polina Muravieva <polinamur3@gmail.com>
2024-09-08 14:27:40 +0300

    form submission and login page validation done

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -1,5 +1,64 @@
 export class Login {
-    constructor() {
-        console.log('LOGIN');
+    constructor(openNewRoute) {
+        this.openNewRoute = openNewRoute;
+
+        this.emailElement = document.getElementById('email');
+        this.passwordElement = document.getElementById('password');
+        this.rememberMeElement = document.getElementById('remember-me');
+        this.commonErrorElement = document.getElementById('common-error');
+
+        document.getElementById('process-button').addEventListener('click', this.login.bind(this));
+
+    }
+
+    validateForm() {
+        let isValid = true;
+        if (this.emailElement.value && this.emailElement.value.match(/^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/)) {
+            this.emailElement.classList.remove('is-invalid');
+        } else {
+            this.emailElement.classList.add('is-invalid');
+            isValid = false;
+        }
+
+        if (this.passwordElement.value) {
+            this.passwordElement.classList.remove('is-invalid');
+        } else {
+            this.passwordElement.classList.add('is-invalid');
+            isValid = false;
+        }
+
+        return isValid;
+    }
+
+    async login() {
+        this.commonErrorElement.style.display = 'none';
+
+        if (this.validateForm()) {
+            const response = await fetch('http://localhost:3000/api/login', {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json',
+                    'Accept': 'application/json',
+                },
+                body: JSON.stringify({
+                    email: this.emailElement.value,
+                    password: this.passwordElement.value,
+                    rememberMe: this.rememberMeElement.checked
+                })
+            });
+
+            const result = await response.json();
+
+            if (result.error || !result.accessToken|| !result.refreshToken|| !result.id|| !result.name) {
+                this.commonErrorElement.style.display = 'block';
+                return;
+            }
+
+            localStorage.setItem('accessToken', result.accessToken)
+            localStorage.setItem('refreshToken', result.refreshToken)
+            localStorage.setItem('userInfo', JSON.stringify({id: result.id, name: result.name}));
+
+            this.openNewRoute('/');
+        }
     }
 }
--- frontend/src/router.js
+++ frontend/src/router.js
@@ -33,7 +33,7 @@ export class Router {
                 load: () => {
                     document.body.classList.add('login-page');
                     document.body.style.height = '100vh';
-                    new Login();
+                    new Login(this.openNewRoute.bind(this));
                 },
                 unload: () => {
                     document.body.classList.remove('login-page');
@@ -63,10 +63,16 @@ export class Router {
     initEvents() {
         window.addEventListener('DOMContentLoaded', this.activateRoute.bind(this));
         window.addEventListener('popstate', this.activateRoute.bind(this));
-        document.addEventListener('click', this.openNewRoute.bind(this));
+        document.addEventListener('click', this.clickHandler.bind(this));
     }
 
-    async openNewRoute(e) {
+    async openNewRoute(url) {
+        const currentRoute = window.location.pathname;
+        history.pushState({},'', url);
+        await this.activateRoute(null, currentRoute);
+    }
+
+    async clickHandler(e) {
         let element = null;
         if (e.target.nodeName === 'A') {
             element = e.target;
@@ -82,9 +88,7 @@ export class Router {
                 return;
             }
 
-            const currentRoute = window.location.pathname;
-            history.pushState({},'', url);
-            await this.activateRoute(null, currentRoute);
+           await this.openNewRoute(url);
         }
     }
 
